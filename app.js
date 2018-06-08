aa4b69fa9437b32a8438a273eced8c6cd5e9befd
uanoob <usnet@ukr.net>
2018-06-08 14:13:07 +0300

    Create http request methods: POST, PUT, DELETE

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
 const http = new easyHTTP();
 
 // Get posts
-http.get('https://jsonplaceholder.typicode.com/posts1', function(err, response) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Get post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Create data
+const data = {
+  title: 'Pure Javascript',
+  body: 'Javascript high ordered function',
+};
+
+// Create post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, response){
+//   if (err) {
+//      console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Update post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, response){
+//   if (err) {
+//      console.log(err);
+//   } else {
+//     console.log(response);
+//   }
+// });
+
+// Delete post
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
   if (err) {
     console.log(err);
   } else {
