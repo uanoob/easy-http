46a702a1fa08f038557e0fcf46b9898859301f50
uanoob <usnet@ukr.net>
2018-06-09 12:13:41 +0300

    Create PUT, DELETE http request method with ES6 Class, Promise, Fetch

--- app-es6.js
+++ app-es6.js
@@ -1,19 +1,31 @@
 const http = new EasyHTTP();
 
 // GET users
-http
-  .get('https://jsonplaceholder.typicode.com/users')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+// http
+//   .get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
 // User data
-const user = {
-  name: 'John',
-  username: 'Doe',
-};
+// const user = {
+//   name: 'John',
+//   username: 'Doe',
+// };
 
 // Create user
+// http
+//   .post('https://jsonplaceholder.typicode.com/users', user)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Update user
+// http
+//   .put('https://jsonplaceholder.typicode.com/users/1', user)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Delete user
 http
-  .post('https://jsonplaceholder.typicode.com/users', user)
+  .put('https://jsonplaceholder.typicode.com/users/10')
   .then(data => console.log(data))
   .catch(err => console.log(err));
--- easyhttp-es6.js
+++ easyhttp-es6.js
@@ -33,4 +33,35 @@ class EasyHTTP {
         .catch(error => reject(error));
     });
   }
+
+  // Make an HTTP PUT request
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+  }
+
+  // Make an HTTP DELETE request
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json',
+        },
+      })
+        .then(response => response.json())
+        .then(data => resolve(data))
+        .catch(error => reject(error));
+    });
+  }
 }
